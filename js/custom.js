f7379c18f3e34f62c895e873d5596216146a5cd2
aswinavofficial <aswinavofficial@gmail.com>
2018-03-16 00:00:48 +0530

    loader and eventbrite

--- js/custom.js
+++ js/custom.js
@@ -229,7 +229,7 @@ $(function(){
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/bg3.png",
+       "images/bg4.jpg",
        "images/s4.jpg",
 	   "images/s4.jpg",
        "images/s4.jpg",
